6d3571b8a4170d14f009b23559f618c72feb43bf
Gabriel Rosa <grosa.ict@gmail.com>
2021-06-20 13:32:03 -0300

    Testing deploy V6

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { resolve } = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, resolve)));
+app.use('/',
+        express.static( resolve(__dirname, './build') )
+);
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
