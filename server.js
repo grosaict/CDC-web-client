6368bce302e9fc84f3b406e942b5f6ad21338f98
Gabriel Rosa <grosa.ict@gmail.com>
2021-06-20 15:13:00 -0300

    Routes fail analysis

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+
+//              >>>   https://www.luiztools.com.br/post/como-publicar-aplicacao-reactjs-na-heroku/
+
 const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './build')));
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
+
+
+//              >>>   https://www.youtube.com/watch?v=S0_WcPP83z8
+
+/* const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
 
-app.use('/*',
+app.use('/',
         express.static( resolve(__dirname, './build') )
 );
 
@@ -13,19 +31,4 @@ app.listen(process.env.PORT || 3000, (err) => {
         console.log(err)
     }
   console.log('Aplicação iniciada');
-});
-
-
-// https://www.luiztools.com.br/post/como-publicar-aplicacao-reactjs-na-heroku/
-
-/* const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 3000);
-
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
 }); */
