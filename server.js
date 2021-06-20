c8c90aa6114cfc79e1601c0776be1f61ee6f173a
Gabriel Rosa <grosa.ict@gmail.com>
2021-06-20 13:26:10 -0300

    Testing deploy V5

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("CADERNETA DA CRIANCA")
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, resolve)));
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        console.log("server.js > err >>>")
+        console.log(err)
+    }
+  console.log('Aplicação iniciada');
+});
+
+
+// https://www.luiztools.com.br/post/como-publicar-aplicacao-reactjs-na-heroku/
+
+/* const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+}); */
